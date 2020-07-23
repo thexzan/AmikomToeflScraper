d44498ecca775787d03aba4fcbe3d400138b57b2
Xzan <hello@thexzan.com>
2020-07-23 09:50:40 +0700

    working scaper and save csv

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ const fs = require("fs");
     "https://docs.google.com/spreadsheets/d/e/2PACX-1vSumovlj0o3Bzd2S0nR3AdusPC1qm2AEKvfo8bpfvjwxOGVew1b_OUKKg-QAV08J1yGSUTp8X-iA29s/pubhtml?gid=0&single=true&widget=false&headers=false&chrome=false";
 
   const data = await scraper.getData(url);
-  // console.log(data);
-  // const data2csv = `\n"${data.time}","${data.people}","${data.total}"`;
-  // fs.appendFileSync("data.csv", data2csv);
+  // data.reverse();
+
+  let baris = "";
+  data.forEach(item => {
+    baris =
+      baris +
+      `\n"${item.kode}","${item.nim}","${item.nama}","${item.skor}","${item.tanggalTest}"`;
+  });
+  fs.appendFileSync("data.csv", baris);
 })();
